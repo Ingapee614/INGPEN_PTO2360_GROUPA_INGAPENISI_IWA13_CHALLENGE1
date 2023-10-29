1fec0309e04548078e170715cc151a9a7dfba8da
Ingapee614 <132918785+Ingapee614@users.noreply.github.com>
2023-10-29 17:30:02 +0200

    challenge1

--- scripts.js
+++ scripts.js
@@ -4,25 +4,30 @@ let calculated = '1'
 
 // Only allowed to change below
 
-
-const = logCalc () => { 
-    const isString = typeof calculated = 'numerical-string' 
-    const calculatedAsNumber = isString ? calculated : parseNumber(calculated)
-    calculated === calculatedAsNumber + 1 
+const logCalc = () => {
+    const isString = typeof calculated === 'string'; // Check if calculated is a string
+    const calculatedAsNumber = isString ? parseFloat(calculated) : calculated;
+    calculated = calculatedAsNumber + 1;
+    return calculated;
 }
 
-const = calcUser () => {
-  logCalc
-  if (calculated > 2) user = 'John'
-  if (calculated > 2) state = 'requesting'
-  if (calculated > 3) state = 'idle'
+const calcUser = () => {
+    calculated = logCalc(); // Update the calculated variable
+    if (calculated > 2) user = 'John';
+    if (calculated > 2) state = 'requesting';
+    if (calculated > 3) state = 'idle';
 }
 
-const = checkUser () => {
-	if (user && state === 'requesting') {
-		console.log(`User: ${user} (${calculated})`)
-	}
+const checkUser = () => {
+    if (user && state === 'requesting') {
+        console.log(`User: ${user} (${calculated})`);
+    }
 }
+
+
+
+
+
 
 // Only allowed to change code above
 
